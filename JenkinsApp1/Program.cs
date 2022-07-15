cebf2383a472e51cbb91edd3755a64f02738350a
Abhinav-3009 <abhi30092000.ag@gmail.com>
2022-07-15 15:34:28 +0530

    added  two methods

--- JenkinsApp1/Program.cs
+++ JenkinsApp1/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using MyMaths;
 namespace JenkinsApp1
 {
     class Program
@@ -7,6 +7,14 @@ namespace JenkinsApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("enter 1 number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter 2 number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            MyMathsClass myMathsClass = new MyMathsClass();
+            Console.WriteLine("addition = " + myMathsClass.Add(a, b));
+            Console.WriteLine("addition = " + myMathsClass.Mul(a, b));
+
             Console.ReadLine();
         }
     }
